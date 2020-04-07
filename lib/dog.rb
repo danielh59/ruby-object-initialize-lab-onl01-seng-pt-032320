da1b8f2fa3255ef0616e985ddd8b14dfe4c0af0e
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-07 18:44:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,9 @@ class Dog
   def initialize(name, breed)
     @name = name 
     @breed = breed 
-    if breed = 0 
-       "Mutt"
      end
+      if @breed = 0 
+      puts "Mutt"
   end
     
 end 
